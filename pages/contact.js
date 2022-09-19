aef485de3abc10cffdfd221fa8d8c171d652eb72
NasirMuh <octobernine1995@gmail.com>
2022-09-19 05:36:36 +0500

    firebase contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,54 +1,73 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Contact = () => {
-    const [name, setName] = useState('');
-    const SubmitHandler = (event) => {
-        console.log(e.value)
-        event.preventDefault();
-    }
+    const [user, setUser] = useState({ name: "", email: "" });
+    const [getUser, setGetUser] = useState([]);
+
     const InputHandler = (event) => {
-        if (event.target.name == 'name') {
-            console.log(event.target.value)
-            setName(event.target.value)
-        }
+        setUser({ ...user, [event.target.name]: event.target.value })
     }
+
+    const SubmitHandler = async (event) => {
+        event.preventDefault();
+        const { name, email } = user;
+        const res = await fetch('https://firstfirebasedb-e0bf7-default-rtdb.firebaseio.com/reactform.json',
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name, email }),
+            }
+        );
+        if (res) { setUser({ name: "", email: "" }) }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('https://firstfirebasedb-e0bf7-default-rtdb.firebaseio.com/reactform.json')
+            const data = await res.json();
+            setGetUser([data]);
+        }
+        fetchData();
+    }, [])
 
     return (
         <>
-           <Head>
+            <Head>
                 <title>Contact Us</title>
                 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" />
             </Head>
-
+            {
+                getUser.map((dd, ind) => {
+                    const { email, name } = dd.["Students"];
+                    return <h1 key={ind}>Name : {name}  Email : {email}</h1>
+                })
+            }
             <section className="text-gray-600 body-font relative">
                 <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col text-center w-full mb-12">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
-                    </div>
+
                     <div className="lg:w-1/2 md:w-2/3 mx-auto">
                         <div className="flex flex-wrap -m-2">
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input value={user.name} onChange={InputHandler} type="text" id="name" name="name" placeholder='Name' className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                                    <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input value={user.email} onChange={InputHandler} type="email" id="email" name="email" placeholder='Email' className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div className="p-2 w-full">
+                            {/* <div className="p-2 w-full">
                                 <div className="relative">
                                     <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
                                     <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                 </div>
-                            </div>
+                            </div> */}
                             <div className="p-2 w-full">
-                                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                                <button onClick={SubmitHandler} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
                             </div>
                             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                                 <a className="text-indigo-500">example@email.com</a>
